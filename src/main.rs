6a43f8ccac900e2cd96194b635beadb827cae59d
ARez2 <dark.gaming@fantasymail.de>
2024-01-10 09:59:30 +0100

    Clean up main entry point

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
 use geese::*;
 use granular_core::{GranularEngine, events, EventLoopSystem};
-use log::info;
+use log::{info, trace};
 
 fn main() {
     std::env::set_var("RUST_BACKTRACE", "1");
     std::env::set_var("RUST_LOG", "granular,wgpu=debug");
     env_logger::init();
-    //ctx.flush().with(geese::notify::add_system::<EventLoopSystem>());
-    //ctx.flush().with(geese::notify::add_system::<Game>());
 
     let window_size = Some(winit::dpi::PhysicalSize::new(640, 480));
 
     let mut engine = GranularEngine::new();
     engine.get_ctx().flush().with(geese::notify::add_system::<Game>());
-    engine.create_window("My game", window_size);
-
-    // let mut engine = ctx.get_mut::<GranularEngine>();
+    engine.create_window("Granular", window_size);
     engine.run();
-    // let mut event_loop_sys = ctx.get_mut::<EventLoopSystem>();
-    // let event_loop = event_loop_sys.take();
-    // drop(event_loop_sys);
-    // event_loop.run(move |event, target| {
-    //     ctx.flush().with(event);
-    //     let mut engine = ctx.get_mut::<GranularEngine>();
-    //     engine.use_window_target(target);
-    //     engine.update();
-    // }).unwrap();
 }
 
 
@@ -34,7 +21,7 @@ struct Game {
 }
 impl Game {
     fn on_update(&mut self, event: &events::NewFrame) {
-        //info!("Update game");
+        trace!("Update game");
     }
 }
 impl GeeseSystem for Game {
